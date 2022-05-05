f1929043c2e72f5adf2fee1ac9aabc412ae15950
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-05 12:01:44 +0100

    trying to solve lighthouse error

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
     ],
   },
   plugins: [
