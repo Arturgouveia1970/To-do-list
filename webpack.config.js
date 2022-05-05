5a4c2187be858cbad9a5c02810e7a61572c0aac8
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-05 16:19:54 +0100

    removed

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   output: {
-    filename: '[main].js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
